a017c9e9659975fe0203630e0a48ce801e8cfaf1
Otavie <otavieokuoyo@gmail.com>
2024-03-31 18:57:40 +0100

    add class methods

--- objects.js
+++ objects.js
@@ -3,7 +3,17 @@ class User {
         this.email = email;
         this.name = name;
     }
+    login () {
+        console.log(this.email, 'just logged in')
+    }
+    logout() {
+        console.log(this.email, 'just logged out!')
+    }
 }
+
 
 var userOne = new User('otavieokuoyo@gmail.com', 'Otavie')
 var userTwo = new User('peace@gmail.com', 'Peace')
+
+userOne.logout()
+userTwo.login()
