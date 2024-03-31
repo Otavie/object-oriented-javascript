f3ed9ae7504537fdd785c1fc9c373ff68b363a65
Otavie <otavieokuoyo@gmail.com>
2024-03-31 18:41:20 +0100

    Add title to index and modified objects.js

--- objects.js
+++ objects.js
@@ -1,7 +1,14 @@
 class User {
-    constructor() {
-
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
     }
 }
 
-var userOne = new User()
+var userOne = new User('otavieokuoyo@gmail.com', 'Otavie')
+var userTwo = new User('peace@gmail.com', 'Peace')
+
+// The 'new' keyword 
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
