ea149fe4c6aad5cf078d2ca04a84417d378a0a04
Otavie <otavieokuoyo@gmail.com>
2024-04-01 10:55:45 +0100

    Add unique code

--- objects.js
+++ objects.js
@@ -1,42 +1,16 @@
 // Constructor Under the Hood
+function User(email, name) {
+    this.email = email;
+    this.name = name;
+    this.online = false;
 
-class User {
-    constructor(email, name) {
-        this.email = email;
-        this.name = name;
-        this.score = 0
-    }
-    login () {
-        console.log(this.email, 'just logged in')
-        return this;
-    }
-    logout() {
-        console.log(this.email, 'just logged out!')
-        return this;
-    }
-    updateScore() {
-        this.score++;
-        console.log(this.email, 'score is now', this.score)
-        return this;
+    this.login = function() {
+        console.log(this.email, 'has logged in!')
     }
 }
-
-class Admin extends User {
-    deleteUser(userParam) {
-        users = users.filter((user) => {
-            return user.email !== userParam.email
-        })
-    }
-}
-
 
 var userOne = new User('otavie@gmail.com', 'Otavie')
 var userTwo = new User('peace@gmail.com', 'Peace')
-var admin = new Admin('otavieokuoyo@gmail.com', 'Otavie Okuoyo')
 
-var users = [userOne, userTwo, admin]
-
-
-admin.deleteUser(userOne)
-
-console.log(users)
+console.log(userOne)
+userTwo.login()
